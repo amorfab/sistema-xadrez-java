59ae3c25fc9fc651d7c77e7af21945b43852e33f
amorfab <fabiana_de_amorim@hotmail.com>
2024-03-25 17:06:10 -0300

    Colocando pecas no tabuleiro

--- src/jogatabuleiro/Tabuleiro.java
+++ src/jogatabuleiro/Tabuleiro.java
@@ -34,4 +34,8 @@ public class Tabuleiro {
         return pecas[posicao.getLinha()][posicao.getColuna()];
     }
 
+    public void LugarDaPeca (Peca peca, Posicao posicao){
+        pecas[posicao.getLinha()][posicao.getColuna()] = peca;
+        peca.position = posicao;
+    }
 }
--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -1,12 +1,16 @@
 package xadrez;
 
+import jogatabuleiro.Posicao;
 import jogatabuleiro.Tabuleiro;
+import xadrez.pecas.Rei;
+import xadrez.pecas.Torre;
 
 public class PartidaXadrez {
     private Tabuleiro tabuleiro;
 
     public PartidaXadrez() {
         this.tabuleiro = new Tabuleiro(8,8);
+        SetupInicial();
     }
 
     public PecaXadrez[][] getPecas(){
@@ -17,5 +21,11 @@ public class PartidaXadrez {
             }
         }
         return mat;
+    }
+
+    private void SetupInicial(){
+        tabuleiro.LugarDaPeca(new Torre(tabuleiro, Cor.BRANCA), new Posicao(2,1));
+        tabuleiro.LugarDaPeca(new Rei(tabuleiro, Cor.PRETA), new Posicao(0,4));
+        tabuleiro.LugarDaPeca(new Rei(tabuleiro, Cor.BRANCA), new Posicao(7,4));
     }
 }
